3432387509a6c49295ff1315225a066133b2bd09
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2022-10-25 17:18:47 -0600

    rename font

--- src/main.rs
+++ src/main.rs
@@ -56,8 +56,8 @@ fn main() -> BError {
         .with_tile_dimensions(32,32)
         .with_resource_path("resources/")
         .with_font("dungeonfont.png", 32,32)
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeon_tiles.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeon_tiles.png")
         .build()?;
 
     main_loop(context, State::new())

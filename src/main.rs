011b61db75d507596d163403b4a1695188f79686
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2022-10-25 16:33:24 -0600

    build map

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod prelude {
 }
 
 use prelude::*;
+use crate::map_builder::MapBuilder;
 
 struct State {
     map: Map,
@@ -21,11 +22,12 @@ struct State {
 
 impl State {
     fn new() -> Self {
+        let mut rng =  RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+
         Self {
-            map: Map::new(),
-            player: Player::new(
-            Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-            ),
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
         }
     }
 }
--- src/map_builder.rs
+++ src/map_builder.rs
@@ -1,14 +1,3 @@
-
-
-mod prelude {
-    pub use bracket_lib::prelude::*;
-    pub const SCREEN_WIDTH: i32 = 80;
-    pub const SCREEN_HEIGHT: i32 = 50;
-    pub use crate::map::*;
-    pub use crate::player::*;
-    pub use crate::map_builder::*;
-}
-
 use crate::prelude::*;
 const NUM_ROOMS: usize = 20;
 

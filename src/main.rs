33b8eb22452b2ba9ef987bcfeee6e375e1e61de0
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2022-10-25 17:19:01 -0600

    rename font

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ fn main() -> BError {
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
         .with_tile_dimensions(32,32)
         .with_resource_path("resources/")
-        .with_font("dungeonfont.png", 32,32)
+        .with_font("dungeon_tiles.png", 32,32)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeon_tiles.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeon_tiles.png")
         .build()?;

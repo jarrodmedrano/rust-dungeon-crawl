4a27351a6b009cc5dd932a742fe6d81ef2988ac0
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2022-10-25 17:22:47 -0600

    rename file

--- src/main.rs
+++ src/main.rs
@@ -55,9 +55,9 @@ fn main() -> BError {
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
         .with_tile_dimensions(32,32)
         .with_resource_path("resources/")
-        .with_font("dungeon_tiles.png", 32,32)
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeon_tiles.png")
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeon_tiles.png")
+        .with_font("dungeonfont.png", 32,32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())

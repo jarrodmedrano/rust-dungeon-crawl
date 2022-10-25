ed12af2cff4dc9a4e7bd0998b7e710b66e9ee516
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2022-10-25 17:14:36 -0600

    add moving camera

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@
 mod map;
 mod player;
 mod map_builder;
+mod camera;
 
 mod prelude {
+    pub use crate::map::*;
+    pub use crate::player::*;
+    pub use crate::camera::*;
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
-    pub use crate::map::*;
-    pub use crate::player::*;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
 }
 
 use prelude::*;
@@ -18,6 +22,7 @@ use crate::map_builder::MapBuilder;
 struct State {
     map: Map,
     player: Player,
+    camera: Camera
 }
 
 impl State {
@@ -28,6 +33,7 @@ impl State {
         Self {
             map: map_builder.map,
             player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start)
         }
     }
 }
@@ -35,9 +41,10 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        self.map.render(ctx);
-        self.player.update(ctx, &self.map);
-        self.player.render(ctx);
+        self.map.render(ctx, &self.camera);
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.player.render(ctx, &self.camera);
+
     }
 }
 
@@ -45,6 +52,12 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32,32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32,32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())
